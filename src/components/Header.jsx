b78dc85ce5b3c752f3ecbcb145ae33cd70891b26
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-03-29 16:50:13 -0300

    Basic project and Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import styles from "./header.module.css";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <h1 className={styles.logo}>
+              Tarefas<span>+</span>
+            </h1>
+          </Link>
+        </nav>
+        <button className={styles.loginButton}>Logar</button>
+      </section>
+    </header>
+  );
+}
+
+export default Header;
